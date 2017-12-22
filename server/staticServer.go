265e43311586709702d3921c26344d501f7a7041
JIANGWEILONGGG <william@jwlchina.cn>
2017-12-22 20:24:03 +0800

    echo server

---
+++ server/staticServer.go
@@ -0,0 +1,38 @@
+package server
+
+
+import (
+	"net/http"
+	"fmt"
+	"errors"
+	"bytes"
+	"html/template"
+)
+
+
+func StaticHandler(res http.ResponseWriter, req *http.Request) {
+	t, err := template.ParseFiles("I:\\ubuntu14.04\\share\\docker\\go\\www\\src\\github.com\\kong36088\\danmu\\client\\index.html")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = WriteTemplateToHttpResponse(res, t)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+func WriteTemplateToHttpResponse(res http.ResponseWriter, t *template.Template) error {
+	if t == nil || res == nil {
+		return errors.New("WriteTemplateToHttpResponse: t must not be nil.")
+	}
+	var buf bytes.Buffer
+	err := t.Execute(&buf, nil)
+	if err != nil {
+		return err
+	}
+	res.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err = res.Write(buf.Bytes())
+	return err
+}
--- server/wsServer.go
+++ server/wsServer.go
@@ -5,9 +5,6 @@ import (
 	"net/http"
 	"log"
 	"fmt"
-	"html/template"
-	"errors"
-	"bytes"
 )
 
 var clients = make(map[*websocket.Conn]bool) // connected clients
@@ -15,6 +12,9 @@ var broadcast = make(chan Message)           // broadcast channel
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	}, // 解决域不一致的问题
 }                                            // 将http升级为websocket
 
 type Message struct {
@@ -26,6 +26,7 @@ func onConnect(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
+
 		return
 	}
 	for {
@@ -43,35 +44,8 @@ func onConnect(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func HomePage(res http.ResponseWriter, req *http.Request) {
-	t, err := template.ParseFiles("I:\\ubuntu14.04\\share\\docker\\go\\www\\src\\github.com\\kong36088\\danmu\\server\\client\\index.htmlgit ")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	err = WriteTemplateToHttpResponse(res, t)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-}
-
-func WriteTemplateToHttpResponse(res http.ResponseWriter, t *template.Template) error {
-	if t == nil || res == nil {
-		return errors.New("WriteTemplateToHttpResponse: t must not be nil.")
-	}
-	var buf bytes.Buffer
-	err := t.Execute(&buf, nil)
-	if err != nil {
-		return err
-	}
-	res.Header().Set("Content-Type", "text/html; charset=utf-8")
-	_, err = res.Write(buf.Bytes())
-	return err
-}
-
 func StartServer() {
-	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/", StaticHandler)
 	http.HandleFunc("/ws", onConnect)
 	err := http.ListenAndServe(":9500", nil)
 	if err != nil {
