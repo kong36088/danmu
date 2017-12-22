522be4e796e555d308f566e920339e93386c9212
JIANGWEILONGGG <william@jwlchina.cn>
2017-12-23 00:25:03 +0800

    完成异步改造，加入goroutine

--- server/proto.go
+++ server/proto.go
@@ -1,1 +1,24 @@
 package server
+
+import (
+	"encoding/json"
+	"log"
+)
+
+type Message struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
+func (msg Message) String() string {
+	return msg.JsonEncode()
+}
+
+func (msg *Message) JsonEncode() string {
+	j, err := json.Marshal(msg)
+	if err != nil {
+		log.Println(err)
+		return ""
+	}
+	return string(j)
+}
--- server/wsServer.go
+++ server/wsServer.go
@@ -5,8 +5,12 @@ import (
 	"net/http"
 	"log"
 	"fmt"
+	"encoding/json"
+	"sync"
 )
 
+//TODO 心跳检测 断开检测
+//TODO kafka
 var clients = make(map[*websocket.Conn]bool) // connected clients
 var broadcast = make(chan Message)           // broadcast channel
 var upgrader = websocket.Upgrader{
@@ -16,37 +20,55 @@ var upgrader = websocket.Upgrader{
 		return true
 	}, // 解决域不一致的问题
 }                                            // 将http升级为websocket
-
-type Message struct {
-	Type    string `json:"type"`
-	Message string `json:"message"`
-}
+var rwLock sync.RWMutex                      // 读写锁
 
 func onConnect(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
-
 		return
 	}
+	rwLock.Lock()
+	clients[conn] = true
+	rwLock.Unlock()
+	go listenMessage(conn)
+}
+
+func listenMessage(conn *websocket.Conn) {
 	for {
-		messageType, p, err := conn.ReadMessage()
+		var msgs Message
+		_, message, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
 			return
 		}
-		fmt.Println(string(p))
-		fmt.Println(messageType)
-		if err := conn.WriteMessage(messageType, p); err != nil {
+		err = json.Unmarshal([]byte(message), &msgs)
+		if err != nil {
 			log.Println(err)
-			return
 		}
+		fmt.Println(msgs)
+		broadcast <- msgs
+	}
+}
+
+func messagePusher() {
+	for {
+		msg := <-broadcast
+		rwLock.RLock()
+		for conn, _ := range clients {
+			//TODO messageType
+			conn.WriteMessage(1, []byte(msg.JsonEncode()))
+		}
+		rwLock.RUnlock()
 	}
 }
 
 func StartServer() {
 	http.HandleFunc("/", StaticHandler)
 	http.HandleFunc("/ws", onConnect)
+
+	go messagePusher()
+
 	err := http.ListenAndServe(":9500", nil)
 	if err != nil {
 		log.Fatal(err)
