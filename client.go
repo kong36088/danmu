0d4081d7312174b93c8328e70ac4f093b64a79a0
JIANGWEILONGGG <william@jwlchina.cn>
2018-01-02 23:28:32 +0800

    ClientBucket

--- client.go
+++ client.go
@@ -2,10 +2,20 @@ package danmu
 
 import (
 	"github.com/gorilla/websocket"
+	"sync"
+)
+
+type (
+	cid int64
+)
+
+var (
+	clientBucket *ClientBucket
 )
 
 type Client struct {
-	Conn *websocket.Conn
+	ClientId cid //remain
+	Conn     *websocket.Conn
 }
 
 func (client *Client) WriteErrorMsg(msg string) error {
@@ -33,4 +43,41 @@ func (client *Client) Write(proto Proto) error {
 
 func (client *Client) Close() {
 	client.Conn.Close()
+}
+
+type ClientBucket struct {
+	Clients map[*websocket.Conn]*Client
+	lck     sync.RWMutex
+	Room    *Room
+}
+
+func InitClientBucket() {
+	clientBucket = &ClientBucket{
+		Clients: make(map[*websocket.Conn]*Client),
+		lck:     sync.RWMutex{},
+	}
+}
+
+func (cb *ClientBucket) Get(conn *websocket.Conn) (*Client, error) {
+	if v, ok := cb.Clients[conn]; ok {
+		return v, nil
+	} else {
+		return nil, ErrRoomDoesNotExist
+	}
+
+}
+
+func (cb *ClientBucket) Add(client *Client) error {
+	cb.lck.Lock()
+	cb.Clients[client.Conn] = client
+	cb.lck.Unlock()
+	return OK
+}
+
+func (cb *ClientBucket) Remove(conn *websocket.Conn) error {
+	if _, ok := cb.Clients[conn]; !ok {
+		return ErrRoomDoesNotExist
+	}
+	delete(cb.Clients, conn)
+	return OK
 }
